560970e891190665ea2b02bb4bd249d77e21228d
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-05-10 19:01:36 +0000

    First commit

---
+++ src/main/java/DAO/AccountDao.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+// Create a DAO classes for each table in the SocialMedia.sql database.
+// Implement the CRUD (Create, Read, Update, Delete) operartions for the Account table.
+// In each method create a PreparedStatement object
+
+public class AccountDao{
+    // Establish connection with the database
+    private final Connection conn = ConnectionUtil.getConnection();
+
+    // Create a new account
+
+    // Get account by id
+
+    // Update account
+
+    // Delete account
+
+}
---
+++ src/main/java/DAO/MessageDao.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class MessageDao {
+    private final Connection conn = ConnectionUtil.getConnection();
+
+    // Create a new message
+
+    // Get message by id
+
+    // Update message
+
+    // Delete message
+    
+}
