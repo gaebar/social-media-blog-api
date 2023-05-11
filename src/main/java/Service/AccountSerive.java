b739f41d81e7e5226db6d59135c8dc1f608f4c6e
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-05-11 04:44:19 +0000

    Start working on the Service classes

--- src/main/java/Service/AccountSerive.java
+++ src/main/java/Service/AccountSerive.java
@@ -1,5 +1,28 @@
 package Service;
 
+import Model.Account;
+import DAO.AccountDao;
+
+import java.util.List;
+
+/* The purpose of a Service class is to contain "business logic" that sits between the web layer (controller) 
+    and persistence layer (DAO). 
+ */
+
 public class AccountSerive {
-    // TODO
+    private AccountDao accountDao;
+
+    public AccountDao(){
+        accountDao = new AccountDao();
+    }  
+
+    public AccountSerive(AccountDao accountDao){
+        this.accountDao = accountDao;
+    }
+
+    public List<Account> getAll(){
+        return accountDao.getAll();
+    }
+
+    // TODO ...
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,10 @@
 package Service;
 
+import Model.Message;
+import DAO.MessageDao;
+
+import java.util.List;
+
 public class MessageService {
     // TODO
 }
