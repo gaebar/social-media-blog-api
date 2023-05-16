1b7379a37401f2f7043552ae0560636a8ffa1412
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-05-16 21:28:23 +0000

    Added comments to Dao and Service exceptions

--- src/main/java/DAO/DaoException.java
+++ src/main/java/DAO/DaoException.java
@@ -1,14 +1,30 @@
 package DAO;
 
-// This is a custom exception class that is thrown when an error occurs in the
-// DAO
+// The DaoException class is a custom exception that is thrown when an error occurs in the DAO layer.
+// By creating this custom exception, we can encapsulate and handle specific errors that may occur
+// in the DAO layer separately from other types of exceptions.
+
+// Extending RuntimeException allows DaoException to be an unchecked exception, meaning it
+// does not need to be declared in method signatures or explicitly caught in code.
 public class DaoException extends RuntimeException {
+
+    // The serialVersionUID is a version identifier for the Serializable class. It
+    // is used during the deserialization process to verify that the sender and
+    // receiver of a serialized object have loaded classes for that object that are
+    // compatible with respect to serialization. If the serialVersionUID values of
+    // the sender and receiver classes are different, deserialization will result in
+    // an InvalidClassException.
     private static final long serialVersionUID = 1L;
 
+    // Constructs a new DaoException with the specified error message.
+    // @param message the error message
     public DaoException(String message) {
         super(message);
     }
 
+    // Constructs a new DaoException with the specified error message and cause.
+    // @param message the error message
+    // @param cause the cause of the exception
     public DaoException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/Service/ServiceException.java
+++ src/main/java/Service/ServiceException.java
@@ -1,20 +1,26 @@
 package Service;
 
-/*
-    ServiceException is a custom exception used within the service layer.
-    It is used to wrap and re-throw exceptions that allows for more specific error handling
-    and message passing,
- */
+// ServiceException is a custom exception used within the service layer.
+// It is used to wrap and re-throw exceptions that allows for more specific error handling
+// and message passing.
 
-public class ServiceException extends RuntimeException{
+// The ServiceException class extends RuntimeException, which is not typically serialized.
+// Additionally, unlike the DaoException class, the ServiceException class does not introduce
+// any new fields or change the serialization behavior of its superclass.
+// Therefore, adding a serialVersionUID is not necessary in this case.
+public class ServiceException extends RuntimeException {
 
     // Constructor that takes in a custom message for the exception
-    public ServiceException(String message){
-    super(message);
+    // @param message the error message
+    public ServiceException(String message) {
+        super(message);
     }
 
-    // Constructor that takes in a custom message and the original exception that causes this exception
-    public ServiceException(String message, Throwable cause){
+    // Constructor that takes in a custom message and the original exception that
+    // causes this exception
+    // @param message the error message
+    // @param cause the cause of the exception
+    public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
 }
